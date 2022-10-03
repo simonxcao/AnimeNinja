6a7b8c6419a19f424c54ba5ab2f613c3ec0f387c
Simon Cao <96557219+simonxcao@users.noreply.github.com>
2022-10-02 23:47:37 -0700

    Add files via upload

---
+++ public/explorecode.js
@@ -0,0 +1,25 @@
+// import { retrieveAnime } from "../app"
+
+// const imageContainer = document.getElementById('image-container')
+
+// let photosArray = retrieveAnime(10)
+// console.log(photosArray)
+
+
+
+
+
+
+// window.addEventListener('scroll', () => {
+//     if (window.innerHeight + window.scrollY <= document.body.offsetHeight - 1000) {
+//         start += 10
+//         end += 10
+//         console.log(start)
+//         console.log(end)
+//         // MoreAnime(start, end)
+
+//     }
+
+// })
+
+
---
+++ user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema
+
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    favs: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Anime',
+            unique: true
+
+        }
+    ]
+})
+
+
+UserSchema.plugin(passportLocalMongoose)
+
+
+module.exports = mongoose.model('User', UserSchema)
